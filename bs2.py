b2aec945de21bdb0e00c9f312ed5f3e08d3d3ba7
kb7 <kubo20231020@gmail.com>
2024-02-27 13:21:41 +0900

    bs2.py 追加

---
+++ bs2.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://joytas.net/kaba/')
+res.encoding = res.apparent_encoding
+
+# 第1引数: html文字列、第2引数: パーサー指定 (今回はライブラリ不要な 'html.parser')
+soup = BeautifulSoup(res.text, 'html.parser')
+
+# BeautifulSoup オブジェクトを print に引き渡すと全文表示
+# print(soup)
+
+# タグで要素取得
+element = soup.find('title')  # <title>コビトカバ</title>
+print(element.text)  #コビトカバ
